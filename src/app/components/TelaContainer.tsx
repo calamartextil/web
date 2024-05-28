cc995571af0745cba432959af1669712324e3bda
Lean Amaro <leanamaro@gmail.com>
2024-05-28 16:20:46 -0300

    added input for tela meters

--- src/app/components/TelaContainer.tsx
+++ src/app/components/TelaContainer.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { useState } from 'react';
 import { Tela } from '@/types';
 import Image from 'next/image';
 import { useCartContext } from '@/app/contexts/CartContext';
@@ -9,11 +10,33 @@ interface TelaProps {
 }
 
 export default function TelaContainer({ tela }: TelaProps) {
+  const [mts, setMts] = useState<number>(1);
+  const [price, setPrice] = useState<number>(2 * tela?.price);
   const { addCartItem } = useCartContext();
 
+  const priceFirstStep = 0.8;
+  const priceSecondStep = 0.5;
+
   const handleAddToCart = () => {
-    addCartItem({ tela, mts: 5, price: 100, estampas: [] });
+    addCartItem({ tela, mts, price: 100, estampas: [] });
   };
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setMts(e.target.value ? parseFloat(e.target.value) : 1);
+    setPrice(
+      e.target.value
+        ? parseFloat(e.target.value) *
+            tela?.price *
+            2 *
+            (parseFloat(e.target.value) > 20
+              ? priceSecondStep
+              : parseFloat(e.target.value) > 10
+              ? priceFirstStep
+              : 1)
+        : 2 * tela?.price
+    );
+  };
+
   return (
     <div className='bg-primary-bg-color py-5 px-6 rounded-2xl'>
       <div className='grid'>
@@ -31,8 +54,25 @@ export default function TelaContainer({ tela }: TelaProps) {
           <div className='flex flex-col justify-center items-start'>
             <div className='relative w-56 h-56 mb-5'>
               <h3 className='text-xl'>{tela?.title}</h3>
-              <p>Desde ${tela?.price}</p>
+              <p>Precio por 1/2 metro: ${tela?.price}</p>
+              <p>Más de 10 metros: ${tela?.price * priceFirstStep}</p>
+              <p>Más de 20 metros: ${tela?.price * priceSecondStep}</p>
               <p>{tela?.description}</p>
+            </div>
+            <div className='flex items-center'>
+              <label htmlFor='mts'>Metros</label>
+              <input
+                type='number'
+                step='0.5'
+                min='1'
+                max='30'
+                id='mts'
+                value={mts}
+                onChange={(e) => handleInputChange(e)}
+              />
+            </div>
+            <div>
+              <p>Total: ${price}</p>
             </div>
             <button onClick={handleAddToCart} className='btn btn-primary'>
               Agregar al carrito
