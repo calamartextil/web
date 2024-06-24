d432829ae6f775b5052b35e05afd590d2363c8de
Lean Amaro <leanamaro@gmail.com>
2024-06-24 18:55:57 -0300

    fix graph for wsp icon

--- src/app/components/WspIcon.tsx
+++ src/app/components/WspIcon.tsx
@@ -6,7 +6,7 @@ import Link from 'next/link';
 export default function WspIcon({ isScroll = false }: { isScroll?: boolean }) {
   return (
     <Link
-      className='fixed bottom-4 right-7 z-20 bg-[#ece5dd] rounded-full p-3 hover:opacity-80 drop-shadow-[0_8px_4px_rgba(0,0,0,.3)]'
+      className='fixed bottom-4 right-3 lg:right-7 z-20 bg-[#ece5dd] rounded-full p-3 hover:opacity-80 drop-shadow-[0_8px_4px_rgba(0,0,0,.3)]'
       target='_blank'
       href='https://api.whatsapp.com/send?phone=5491123644913&text=Quiero%20hacer%20una%20consulta'
     >
